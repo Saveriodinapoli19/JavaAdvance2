efb3bfa3471dfcbfe847c73b4c38da3de81b5f35
Saverio <saveriodinapoli99@gmail.com>
2023-11-23 01:03:03 +0100

    Esercizio13

--- src/Esercizio13/Esercizio13.java
+++ src/Esercizio13/Esercizio13.java
@@ -8,18 +8,17 @@ import java.util.Map;
 public class Esercizio13 {
     public static void main(String[] args) {
         String art1 = "Dado";
-        String art2 = "Penna";
         String art3 = "Cane";
 
         Map<String, String> articolo1 = new HashMap<>();
-        articolo1.put("Oggetto", art1);
+        articolo1.put("Oggetto ", art1 );
         System.out.println(articolo1);
 
-        Map<String, String> articolo2 = Map.of("Cancelleria", art2);
+        Map<String, String> articolo2 = Map.of("Cancelleria ", " Penna");
         System.out.println(articolo2);
 
         Map<String, String> articolo3 = (Map.ofEntries(
-                new AbstractMap.SimpleEntry<>("Animale", art3)));
+                new AbstractMap.SimpleEntry<>("Animale ",  art3)));
         System.out.println(articolo3);
 
 
